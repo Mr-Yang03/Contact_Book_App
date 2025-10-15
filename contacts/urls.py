affb576b6014a08e289f470dd6f1a26b113645fc
Admin <duongphucthang200@gmail.com>
2025-10-15 10:37:34 +0700

    Contact CRUD for views

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-
+    path('create/', views.create_contact, name='create_contact'),
+    path('update/<int:contact_id>/', views.update_contact, name='update_contact'),
+    path('delete/<int:contact_id>/', views.delete_contact, name='delete_contact'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,10 +1,76 @@
-from tempfile import template
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from django.template import loader
+from .models import Contact
 
-# Create your views here.
 def home(request):
-    temp = loader.get_template('index.html')
-    return HttpResponse(temp.render())
+    """Display all contacts"""
+    contacts = Contact.objects.all().order_by('-created_at')
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        contacts = contacts.filter(
+            first_name__icontains=search_query
+        ) | contacts.filter(
+            last_name__icontains=search_query
+        ) | contacts.filter(
+            phone_number__icontains=search_query
+        )
+
+    return render(request, 'index.html', {
+        'contacts': contacts,
+        'search_query': search_query
+    })
+
+def create_contact(request):
+    """Create a new contact"""
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        phone_number = request.POST.get('phone_number')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        birthday = request.POST.get('birthday')
+
+        Contact.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            phone_number=phone_number,
+            email=email if email else None,
+            address=address,
+            birthday=birthday if birthday else None
+        )
+        return redirect('home')
+
+    return redirect('home')
+
+def update_contact(request, contact_id):
+    """Update an existing contact"""
+    contact = get_object_or_404(Contact, id=contact_id)
+
+    if request.method == 'POST':
+        contact.first_name = request.POST.get('first_name')
+        contact.last_name = request.POST.get('last_name')
+        contact.phone_number = request.POST.get('phone_number')
+        contact.email = request.POST.get('email')
+        contact.address = request.POST.get('address')
+        contact.birthday = request.POST.get('birthday')
+
+        if not contact.email:
+            contact.email = None
+        if not contact.birthday:
+            contact.birthday = None
+
+        contact.save()
+        return redirect('home')
+
+    return redirect('home')
+
+def delete_contact(request, contact_id):
+    """Delete a contact"""
+    contact = get_object_or_404(Contact, id=contact_id)
+
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('home')
+
+    return redirect('home')
